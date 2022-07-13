42e20674553369b644002da8b927be95d4f1059d
NikolaZivanovic99 <woshapro@gmail.com>
2022-07-13 12:02:09 +0200

    feat: added identity

--- IngSoftwareInternship/MovieLibrary.Web/Program.cs
+++ IngSoftwareInternship/MovieLibrary.Web/Program.cs
@@ -4,9 +4,11 @@ using MovieLibrary.Business.Mapping;
 using MovieLibrary.Business.Services;
 using MovieLibrary.Business.Services.ServiceInterfaces;
 using MovieLibrary.Data.Models;
+using Microsoft.AspNetCore.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("MovieLibraryWebContextConnection") ?? throw new InvalidOperationException("Connection string 'MovieLibraryWebContextConnection' not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,6 +21,9 @@ builder.Services.AddScoped<UserService, UserService>();
 
 builder.Services.AddDbContext<MoviesDataBaseContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MoviesDataBaseContext>();
 
 var mapperConfig = new MapperConfiguration(mc =>
 {
@@ -38,13 +43,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
-
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
 
 app.MapControllerRoute(
     name: "default",
